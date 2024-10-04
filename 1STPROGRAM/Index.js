4b984850cf906daa203b88f79568c45771ba2181
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-10-04 20:42:10 +0530

    update

---
+++ 1STPROGRAM/Index.js
@@ -0,0 +1,58 @@
+// 1. Find the largest and smallest element in an array
+const arr = [16, 5, 8, 10, 65, 3, 77];
+
+console.log(Math.min(...arr), Math.max(...arr));
+
+//for practice role 30 time
+const randm = [11, 33, 90, 67, 901, 23, 6, 77];
+
+console.log(Math.max(...randm), Math.min(...randm));
+
+const xyz = [11, 22, 33, 66, 20, 11, 2, 4, 88];
+console.log(Math.max(...xyz), Math.min(...xyz));
+
+const abc = [1, 3, 77, 2, 89, 2, 44, 55, 2, 1];
+console.log(Math.min(...abc), Math.max(...abc));
+
+const abb = [20, 11, 23, 32, 56, 1, 9, 100];
+console.log(Math.min(...abb), Math.max(...abb));
+
+const bbb = [29, 1, 2, 55, 89, 10, 2, 5];
+
+console.log(Math.min(...bbb), Math.max(...bbb));
+
+const ccc = [19, 3, 5, 60, 220, 70, 10, 100];
+console.log(Math.min(...ccc), Math.min(...ccc));
+
+const ddd = [10, 20, 1, 3, 70, 400, 200, 23];
+
+console.log(Math.min(...ddd), Math.max(...ddd));
+
+const eee = [1, 0, 22, 100, 3, 60, 77, 12];
+
+console.log(Math.min(...eee), Math.max(...eee));
+
+const fff = [10, 30, 1, 33, 0, 23, 1, 45, 9];
+console.log(Math.min(...fff), Math.max(...fff));
+
+const ggg = [10, 4, 50, 77, 88, 29, 10, 30, 40];
+console.log(Math.min(...ggg), Math.max(...ggg));
+
+const aabc = [1, 3, , 10, 20, 1, 0, 3, 0];
+console.log(Math.min(...aabc), Math.max(...aabc));
+
+const bbca = [1, 2, 3, 5, 1, 3, 99, 4, 10];
+console.log(Math.min(...bbca), Math.max(...bbca));
+
+const ccba = [12, 33, 1, 90, 5, 11];
+console.log(Math.min(...ccba), Math.max(...ccba));
+
+const ddc = [2, 12, 23, 1, 4, 5, 80];
+console.log(Math.min(...ddc), Math.max(...ddc));
+
+const eed = [10, 20, 11, 3, 40, 20, 8];
+console.log(Math.min(...eed), Math.max(...eed));
+const ffe = [19, 34, 70, 27, 1, 3, 4, 87];
+console.log(Math.min(...ffe), Math.max(...ffe));
+const gghh = [190283, 27373, 324, 73535, 61];
+console.log(Math.min(...gghh), Math.max(...gghh));
---
+++ 2ndprogram/index.js
@@ -0,0 +1,15 @@
+//2. Reverse an array
+1;
+const arr = [1, 43, 8, 90, 900, 55, 70, 210, 100];
+const data = arr.reverse();
+console.log(data);
+2;
+const a = [26, 19, 274, 63, 93, 25, 27];
+const aa = a.reverse();
+console.log("2:", aa);
+3;
+const b = [1, 2, 3, 4, 5, 6, 7, 8];
+const bb = b.reverse();
+console.log("3", b);
+4;
+const c = [10, 20, 30, 40];
