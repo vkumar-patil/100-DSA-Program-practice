03a6b8a953c81da34dd6f17a2a1e8bd4ced51634
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-10-06 17:22:44 +0530

    chek frequanci program

---
+++ 4rthProgram/index.js
@@ -0,0 +1,65 @@
+//4. Find the frequency of each element in an array
+let arr = [1, 2, 3, 4, 2, 6, 2, 7, 9];
+let frequency = {};
+arr.forEach((x) => (frequency[x] = (frequency[x] || 0) + 1));
+console.log(frequency);
+
+let brr = [1, 2, 3, 1, 2, 3, 4, 5, 6, 5, 7, 78, 8, 9, 9, 5, 7];
+let b = {};
+brr.forEach((e) => (b[e] = (b[e] || 0) + 1));
+console.log(b);
+
+let crr = [27, 3, 4, 5, 7, 4, 4, 6, 7, 2, 4, 5, 6, 6];
+let c = {};
+crr.forEach((e) => (c[e] = (c[e] || 0) + 1));
+console.log(c);
+
+let drr = [4, 5, 6, 7, 89, 23, 56, 4, 5, 67, 2, 4, 6, 90, 6, 5, 44, 3, 3];
+let d = {};
+drr.forEach((e) => (d[e] = (d[e] || 0) + 1));
+console.log(d);
+
+let err = [48, 5, 5, 53, 3, 2, 45, 55, 212, 24, 45, 6, 77, 432, 23, 45, 6, ,];
+let f = {};
+err.forEach((e) => (f[e] = (f[e] || 0) + 1));
+console.log(f);
+
+let frr = [45, 3, 4, 5, 6, 7, 8, 8, 8, 9, 4, 4, 6, 8];
+let g = {};
+frr.forEach((e) => (g[e] = (g[e] || 0) + 1));
+console.log(g);
+
+let grr = [40, 6, 4, 5, 6, 8, 39, 90, 0, 20, 3];
+h = {};
+grr.forEach((e) => (h[e] = (h[e] || 0) + 1));
+console.log(h);
+
+hrr = [4, 5, 6, 7, 5, 5, 57, 7, 8, 9, 9];
+i = {};
+hrr.forEach((e) => (i[e] = (i[e] || 0) + 1));
+console.log(i);
+
+irr = [4, 6, 6, 7, 7, 8];
+j = {};
+irr.forEach((e) => (j[e] = (j[e] || 0) + 1));
+console.log(j);
+
+jrr = [65, 57, 8, 9, 0, 6, 8, 9, 9, 5, 6, 7, 78];
+k = {};
+jrr.forEach((e) => (k[e] = (k[e] || 0) + 1));
+console.log(k);
+
+krr = [4, 5, 6, 7, 5, 3, 4, 5, 6, 7, 8, 9, 4, 4];
+l = {};
+krr.forEach((e) => (l[e] = (l[e] || 0) + 1));
+console.log(l);
+
+lrr = [65, 33, 3, 3, 3, 3, 3, 3, 4, 5, 6, 6, 7];
+m = {};
+lrr.forEach((e) => (m[e] = (m[e] || 0) + 1));
+console.log(m);
+
+mrr = [3, 4, 5, 56, 7, 3, 45, 6, 7, 7, 33, 456, 7, 88, 8];
+n = {};
+mrr.forEach((e) => (n[e] = (n[e] || 0) + 1));
+console.log(n);
