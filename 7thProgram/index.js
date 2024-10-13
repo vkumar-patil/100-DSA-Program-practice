773772aa54a92ec26686c2b62b670950d1fed6df
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-10-13 16:55:47 +0530

    new add union

--- 7thProgram/index.js
+++ 7thProgram/index.js
@@ -1,4 +1,8 @@
 // 8. Find the intersection and union of two arrays
+//8. Find the intersection and union of two arrays
 
 let a = [1, 2, 3];
 let b = [2, 3, 4];
+let union = [...new Set([...a, ...b])];
+let intersection = a.filter(x => b.includes(x));
+console.log("Union:", union, "Intersection:", intersection);
