68df9aa9e8222ea194a195d720c0a9a3cb7f7955
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-10-16 21:44:05 +0530

    string revars

---
+++ 10thProgram/index.js
@@ -0,0 +1,14 @@
+//11. Reverse a string
+
+let s = "hello";
+let r = s.split("").reverse().join("");
+console.log(r);
+
+let t = "vijay";
+let u = t.split("").reverse("").join("");
+console.log(u);
+//let w = s.join("");
+
+let a = "ajay";
+let aa = a.split("").reverse("").join("");
+console.log(aa);
--- 9thProgram/index.js
+++ 9thProgram/index.js
@@ -12,11 +12,41 @@
 // let sumarr = arrr.reduce((a, b) => a + b, 0);
 // console.log(sumNN - sumarr);
 
-let br = [1, 2, 3,4, 5, 6,8,9,10];
+let br = [1, 2, 3, 4, 5, 6, 8, 9, 10];
 let b = br.length + 1;
-console.log(b);
+//console.log(b);
 let bmn = (b * (b + 1)) / 2;
-console.log(bmn);
+//console.log(bmn);
 let bum = br.reduce((a, b) => a + b, 0);
-console.log(bum);
+//console.log(bum);
 console.log(bmn - bum);
+
+let cr = [1, 2, 3, 4, 5, 6, 7, 9, 10];
+let c = cr.length + 1;
+let cum = (c * (c + 1)) / 2;
+let com = cr.reduce((a, b) => a + b, 0);
+console.log(cum - com);
+
+let dr = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11];
+let d = dr.length + 1;
+let dm = (d * (d + 1)) / 2;
+let dom = dr.reduce((a, b) => a + b, 0);
+console.log(dm - dom);
+
+let er = [2, 3, 4, 5, 6, 7, 8];
+let e = er.length + 1;
+let em = (e * (e + 1)) / 2;
+let eom = er.reduce((a, b) => a + b, 0);
+console.log(em - eom);
+
+let fr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12];
+let f = fr.length + 1;
+let fm = (f * (f + 1)) / 2;
+let fom = fr.reduce((a, b) => a + b, 0);
+console.log(fm - fom);
+
+let gr = [1, 2, 4, 5, 6, 7, 8];
+let g = gr.length + 1;
+let gm = (g * (g + 1)) / 2;
+let gom = gr.reduce((a, b) => a + b, 0);
+console.log(gm - gom);
