52f8e6babf7a959c87beb9cd3aae2295f9d6456f
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-10-23 17:04:39 +0530

    update

--- 12thProgram/index.js
+++ 12thProgram/index.js
@@ -5,3 +5,10 @@ let s1 = "listen",
 let sortedS1 = s1.split("").sort().join("");
 let sortedS2 = s2.split("").sort().join("");
 console.log(sortedS1 === sortedS2);
+
+
+let t1 = "listen",
+  t2 = "silent";
+let tortedS1 = t1.split("").sort().join("");
+let tortedS2 = t2.split("").sort().join("");
+console.log(sortedS1 === sortedS2);
