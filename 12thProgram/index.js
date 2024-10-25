927538209e111e428c6890b3fb608158b9c57854
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-10-25 22:05:46 +0530

    update

--- 12thProgram/index.js
+++ 12thProgram/index.js
@@ -12,3 +12,11 @@ let t1 = "listen",
 let tortedS1 = t1.split("").sort().join("");
 let tortedS2 = t2.split("").sort().join("");
 console.log(sortedS1 === sortedS2);
+
+
+//15. Count the number of vowels and consonants in a string
+
+let s = "hello world";
+let vowels = s.match(/[aeiou]/gi)?.length || 0;
+let consonants = s.match(/[bcdfghjklmnpqrstvwxyz]/gi)?.length || 0;
+console.log("Vowels:", vowels, "Consonants:", consonants);
