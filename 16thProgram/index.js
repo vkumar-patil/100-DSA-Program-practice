5be8912aff062795785ec5c7f3905d4c2c3d08c0
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-11-10 20:05:20 +0530

    update

--- 16thProgram/index.js
+++ 16thProgram/index.js
@@ -1,5 +1,36 @@
 // 1. Check if a number is even or odd
 const number = 3;
 function isEvenOrOdd(number) {
-  return number % 2 === 0 ? "Even" : "Odd";
+  //return
+
+  console.log(number % 2 === 0 ? "Even" : "Odd");
 }
+isEvenOrOdd();
+
+const numbers = 4;
+function evenorodd() {
+  console.log(numbers % 2 === 0 ? "even" : "odd");
+}
+evenorodd();
+
+const numberr = 5;
+function EorO() {
+  if (numberr % 2 === true) {
+    //return E;
+    console.log("E");
+  } else {
+    //O;
+    console.log("o");
+  }
+}
+EorO();
+
+const numberrr = 18;
+function EO() {
+  if (numberrr % 2 === true) {
+    console.log("even ok");
+  } else {
+    console.log("odd ok");
+  }
+}
+EO();
