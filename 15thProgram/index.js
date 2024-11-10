898b84db814277bc2c5277ae5c71aa3060562ed9
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-11-10 20:32:03 +0530

    pallindram code program

--- 15thProgram/index.js
+++ 15thProgram/index.js
@@ -1,18 +1,26 @@
 // 20. Longest palindromic substring
-
-function isPalindrome(s) {
-  return s === s.split("").reverse().join("");
-}
-function longestPalindrome(s) {
-  let maxPalin = "";
-  for (let i = 0; i < s.length; i++) {
-    for (let j = i + 1; j <= s.length; j++) {
-      let substr = s.slice(i, j);
-      if (isPalindrome(substr) && substr.length > maxPalin.length) {
-        maxPalin = substr;
-      }
-    }
+const s = "babab";
+function isPalindrome() {
+  //return
+  const k = s.split("").reverse().join("");
+  if (s === k) {
+    console.log("ispallendram");
+  } else {
+    console.log("not a pallendram");
   }
-  return maxPalin;
 }
-console.log(longestPalindrome("babad"));
+
+isPalindrome();
+// function longestPalindrome(s) {
+//   let maxPalin = "";
+//   for (let i = 0; i < s.length; i++) {
+//     for (let j = i + 1; j <= s.length; j++) {
+//       let substr = s.slice(i, j);
+//       if (isPalindrome(substr) && substr.length > maxPalin.length) {
+//         maxPalin = substr;
+//       }
+//     }
+//   }
+//   return maxPalin;
+// }
+// console.log(longestPalindrome("babad"));
