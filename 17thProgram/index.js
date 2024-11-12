0b6045eff646b50f331d843730204925dd0fc922
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-11-12 20:31:08 +0530

    check if a string contains a certain charectar

--- 17thProgram/index.js
+++ 17thProgram/index.js
@@ -1,4 +1,7 @@
 // 2. Check if a string contains a certain character
+const str = "vijay";
+const char = "i";
 function containsCharacter(str, char) {
   return str.includes(char);
 }
+console.log(containsCharacter(str, char));
