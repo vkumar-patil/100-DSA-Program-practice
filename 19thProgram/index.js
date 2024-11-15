403d9b660e5c25a80186b178ea9d985e079f34e2
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-11-15 14:19:38 +0530

    sum of digit

---
+++ 19thProgram/index.js
@@ -0,0 +1,18 @@
+// 5. Sum of digits in a number
+const number = 2313456734;
+function sumOfDigits(number) {
+  return String(number)
+    .split("")
+    .reduce((sum, digit) => sum + Number(digit), 0);
+}
+console.log(sumOfDigits(number));
+
+const numbers = 12345;
+function sumnum() {
+  console.log(
+    String(numbers)
+      .split("")
+      .reduce((sum, digit) => sum + Number(digit), 0)
+  );
+}
+sumnum();
