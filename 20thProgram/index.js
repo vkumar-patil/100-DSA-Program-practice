26a895607d52542e7342aca6622ada2b2ed6834e
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-11-19 23:21:33 +0530

    count vowels length

--- 20thProgram/index.js
+++ 20thProgram/index.js
@@ -1,3 +1,5 @@
-const str="avand"
-function countVowels(str) { return str.match(/[aeiou]/gi)?.length || 0; }
-console.log(countVowels(str))
+const str = "avand";
+function countVowels(str) {
+  return str.match(/[aeiou]/gi)?.length || 0;
+}
+console.log(countVowels(str));
