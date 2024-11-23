e81a3ed10bffcfecf2ff49d06e7c2312fe48849c
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-11-23 15:55:11 +0530

    find the area of a rectangle

---
+++ 23thProgram/index.js
@@ -0,0 +1,7 @@
+// 12. Find the area of a rectangle
+const length = 10;
+const width = 20;
+function areaOfRectangle(length, width) {
+  return length * width;
+}
+console.log(areaOfRectangle(length, width));
