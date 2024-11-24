b1d0aeded9aa153b7a2d46f073addcf8a043365f
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-11-24 12:59:38 +0530

    update

--- 23thProgram/index.js
+++ 23thProgram/index.js
@@ -5,3 +5,11 @@ function areaOfRectangle(length, width) {
   return length * width;
 }
 console.log(areaOfRectangle(length, width));
+
+// 13. Swap two numbers without using a third variable
+let a=20;
+let b=30;
+function swapNumbers(a, b) {
+  [a, b] = [b, a];
+  return { a, b };
+}
