b2cec73a391b52da57209774a55bfd373a371956
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-11-25 18:19:36 +0530

    find positive & nigative

--- 23thProgram/index.js
+++ 23thProgram/index.js
@@ -7,9 +7,15 @@ function areaOfRectangle(length, width) {
 console.log(areaOfRectangle(length, width));
 
 // 13. Swap two numbers without using a third variable
-let a=20;
-let b=30;
+let a = 20;
+let b = 30;
 function swapNumbers(a, b) {
   [a, b] = [b, a];
   return { a, b };
 }
+// 14. Check if a number is positive or negative
+const number = 4;
+function isPositiveOrNegative(number) {
+  return number >= 0 ? "Positive" : "Negative";
+}
+console.log(isPositiveOrNegative(number))
