f2aa16e5a4fd66a6cd160a2e2e851b7ba1de72a8
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-11-25 21:47:27 +0530

    star pattern

---
+++ 24thProgram/index.js
@@ -0,0 +1,15 @@
+// JavaScript Code for Stars Pattern
+function printStars(rows) {
+  for (let i = 1; i <= rows; i++) {
+    console.log(" ".repeat(rows - i) + "*".repeat(2 * i - 1));
+  }
+}
+
+printStars(5);
+
+function print(row) {
+  for (let i = 1; i <= row; i++) {
+    console.log("".repeat(row - i) + "*".repeat(1 * i - 1));
+  }
+}
+print(5);
