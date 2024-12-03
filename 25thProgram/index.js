97a81f339aba556c66f888850fcdc2a8f588ef9e
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-12-03 09:58:58 +0530

    sum off even value

--- 25thProgram/index.js
+++ 25thProgram/index.js
@@ -12,3 +12,17 @@ function gcd(c, d) {
   return d == 0 ? c : gcd(d, c % d);
 }
 console.log(gcd(c, d));
+
+// 22. Find the LCM of two numbers
+
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
+console.log(lcm(a, b));
+
+// 23. Sum of all even numbers in an array
+const arr = [1, 2, 3, 4, 5, 6];
+function sumEven(arr) {
+  return arr.filter((n) => n % 2 === 0).reduce((a, b) => a + b, 0);
+}
+console.log(sumEven(arr));
